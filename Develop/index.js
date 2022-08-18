58dd56da7b9c9abcce59699f11b9f6470a2ffc8a
Kal O <koestreicher4@gmail.com>
2022-08-18 14:50:58 -0500

    initial commit, questions, and prompt working

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,44 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const generatePage = require('./utils/generateMarkdown');
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const promptUser = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'Title',
+            message: 'What is the title of your project? (required)'
+        },
+        {
+            type: 'input',
+            name: 'authorName',
+            message: 'What is your name? (required)'
+        },
+        {
+            type: 'input',
+            name: 'Installation',
+            message: 'How do you install your project? (required)'
+        },
+        {
+            type: 'input',
+            name: 'Usage',
+            message: 'What usage is your project intended for?'
+        },
+        {
+            type: 'input',
+            name: 'Credits',
+            message: 'List colloborators and third party assests'
+        },
+        {
+            type: 'input',
+            name: 'License',
+            message: 'Licensing'
+        },
+        
+    ])
+}
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
@@ -10,4 +47,4 @@ function writeToFile(fileName, data) {}
 function init() {}
 
 // Function call to initialize app
-init();
+promptUser();
