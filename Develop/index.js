8cf462f5dea153f3ef127ed7b51f731914d14ed3
Kal O <koestreicher4@gmail.com>
2022-08-18 15:46:53 -0500

    initilize app and license checkbox

--- Develop/index.js
+++ Develop/index.js
@@ -1,50 +1,68 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
-const generatePage = require('./utils/generateMarkdown');
+const generateMarkdown = require('./utils/generateMarkdown');
 const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const promptUser = () => {
-    return inquirer.prompt([
+const questions = [
         {
+            // Name of project
             type: 'input',
             name: 'Title',
             message: 'What is the title of your project? (required)'
         },
         {
+            // Name of autho
             type: 'input',
             name: 'authorName',
             message: 'What is your name? (required)'
         },
         {
+            // installation input
             type: 'input',
             name: 'Installation',
             message: 'How do you install your project? (required)'
         },
         {
+            // usage input
             type: 'input',
             name: 'Usage',
             message: 'What usage is your project intended for?'
         },
         {
+            // credits input
             type: 'input',
             name: 'Credits',
             message: 'List colloborators and third party assests'
         },
         {
-            type: 'input',
+            // license select
+            type: 'checkbox',
             name: 'License',
-            message: 'Licensing'
-        },
-        
-    ])
-}
+            message: 'Licensing',
+            choices: ['MPL 2.0', 'GNU', 'Apache', 'MIT', 'None'],
+        },  
+    ];
+
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) => {
+        if (err) {
+            return console.log(err);
+        }
+        console.log('write success');
+    });
+};
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions)
+    .then(function (userInput) {
+        console.log(userInput)
+        writeToFile('README.md', generateMarkdown(userInput));
+    });
+};
 
 // Function call to initialize app
-promptUser();
+init();
