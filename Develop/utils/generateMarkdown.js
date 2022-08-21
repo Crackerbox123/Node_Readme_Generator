cd0d110caa41e52924ded1b896dd5014ad5815f7
Kal O <koestreicher4@gmail.com>
2022-08-20 21:37:27 -0500

    toc add

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -65,9 +65,19 @@ function generateMarkdown(data) {
   return `
   ## Title
   ${data.title}
+  ## Badge:
+   ${renderLicenseBadge(data)}
 
-  ## Badge: ${renderLicenseBadge(data)}
- 
+   ## Table of Contents:
+  1. [Description](#description)
+  2. [Installation](#installation)
+  3. [Usage](#usage)
+  4. [Credits](#credits)
+  5. [License](#license)
+  6. [Features](#features)
+  7. [Contirbute](#contribute)
+  8. [Test](#test)
+  9. [Questions](#questions)
   ## Description:
   ${data.description}
   ## Installation:
